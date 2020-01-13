1796afc9183eb6a61ca4fa30466d8a939ea78ac9
Unknown <patrick.kuhs@stud.uni-due.de>
2020-01-13 22:24:45 +0100

    le push lol

--- src/speicherverwaltung/memoryManager/WorstFit.java
+++ src/speicherverwaltung/memoryManager/WorstFit.java
@@ -4,21 +4,23 @@ import speicherverwaltung.processManager.Process;
 
 public class WorstFit extends MemoryManager {
 
-	private int[] memory; //Memory als array
-	private int[][] gapTable = new int [6][2];    // [Lückenanfang] [Lückengröße]
+	private int[] memory; // Memory als array
+	private int[][] gapTable = new int[6][2]; // [Lückenanfang] [Lückengröße]
 	/*
-	 * Constructor baut den Speicher, hier: int array [cell] in der größe size [1024] mit dem Eintrag "-1" -> frei
-	 * kommt ein prozess, wird die ID in die cell geschrieben
-	 * Aktuallisierungen werden iterativ über dem gesammten Speicher gemacht. (soll ja nicht schnell sein)
+	 * Constructor baut den Speicher, hier: int array [cell] in der größe size
+	 * [1024] mit dem Eintrag "-1" -> frei kommt ein prozess, wird die ID in die
+	 * cell geschrieben Aktuallisierungen werden iterativ über dem gesammten
+	 * Speicher gemacht. (soll ja nicht schnell sein)
 	 */
+
 	public WorstFit(int size) {
 		super(size);
 		memory = new int[size];
-		//setzt felder frei
+		// setzt felder frei
 		System.out.println("setting up...");
-		for(int i = 0; i<size; ++i){
+		for (int i = 0; i < size; ++i) {
 			memory[i] = -1;
-			//System.out.print(i + ": " +memory[i] + " |");
+			// System.out.print(i + ": " +memory[i] + " |");
 		}
 		setGapTable();
 		printGapTable();
@@ -26,37 +28,37 @@ public class WorstFit extends MemoryManager {
 	}
 
 	private void printGapTable() {
-		for(int i = 0; i < gapTable.length; i++){
-			System.out.println("gapStart "+ gapTable[i][0] + " GapSize: " + gapTable[i][1]);
+		for (int i = 0; i < gapTable.length; i++) {
+			System.out.println("gapStart " + gapTable[i][0] + " GapSize: " + gapTable[i][1]);
 		}
 	}
 
-	private void setGapTable(){
+	private void setGapTable() {
 		int c = 0;
 		int gapStart = 0;
 		int gapEnd = 0;
 		int gapIndex = 0;
 		boolean toggle = false;
 		int size = getMemorySize();
-		while(c < size){
-			//System.out.print(c + ": " + memory[c] + " |");
-			if(memory[c] == -1 && c < size - 1){
-				//System.out.println("found gap");
-				if(!toggle){
+		while (c < size) {
+			// System.out.print(c + ": " + memory[c] + " |");
+			if (memory[c] == -1 && c < size - 1) {
+				// System.out.println("found gap");
+				if (!toggle) {
 					gapStart = c;
 					gapEnd = gapStart;
 					toggle = true;
-				}else{
-					//System.out.println("same gap");
+				} else {
+					// System.out.println("same gap");
 					gapEnd++;
 				}
-			}else{
-				if (toggle){
+			} else {
+				if (toggle) {
 					System.out.println("gap closed");
 					gapTable[gapIndex][0] = gapStart;
 					gapTable[gapIndex][1] = (c != size - 1) ? (gapEnd - gapStart) : (gapEnd + 1 - gapStart);
 					gapIndex++;
-					if(gapIndex >= gapTable.length){
+					if (gapIndex >= gapTable.length) {
 						System.err.println("GapTable overflow!");
 						return;
 					}
@@ -66,38 +68,67 @@ public class WorstFit extends MemoryManager {
 			c++;
 		}
 	}
-	//reserviert den speicher
+
+	// reserviert den speicher
 	@Override
 	public boolean allocateMemory(Process process) {
 		int pid = process.getId();
 		int psize = process.getSize();
 		int gapStart = getBiggestGap();
-		return false;
+
+		if (gapStart == -1)
+			return false;
+
+		for (int i = 0; i < psize; ++i) {
+			memory[gapStart + i] = pid;
+		}
+
+		return true;
 	}
 
 	private int getBiggestGap() {
-		return 0;
+		int max = 0;
+		for (int i = 0; i < gapTable.length; i++) {
+			if (gapTable[0][1] == 0) {
+				return -1;
+			} else if (gapTable[i][1] > max) {
+				max = gapTable[i][1];
+			}
+		}
+		return max;
 	}
 
-
-	//gibt den speicher eines Prozesses wieder frei
+	// gibt den speicher eines Prozesses wieder frei
 	@Override
 	public void freeMemory(Process process) {
 		// TODO Auto-generated method stub
+
+		int pid = process.getId();
+		int psize = process.getSize();
+		int gapStart = -1;
+		
+		for(int i = 0; i < memory.length; ++i)
+		{
+			if(memory[i] == pid)
+			{
+				gapStart = i;
+			}
+		}
+
+		for (int i = 0; i < psize; ++i) {
+			memory[gapStart + i] = pid;
+		}
 
 	}
 
 	/**
 	 * This function prints out all reserved memory in the following format:
 	 *
-	 * Id 	Name 	Size 	start 	EndPos
-	 * 0	p1		100		0		99
-	 * 1	p2		500		100		599
-	 * .....
+	 * Id Name Size start EndPos 0 p1 100 0 99 1 p2 500 100 599 .....
 	 */
 	@Override
 	public void printAllProcesses() {
-		//blub
+		// blub
 
 	}
 
