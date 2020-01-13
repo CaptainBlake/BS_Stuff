487ad2fcc129234ad8d5b722fe441383ddb5715f
Captain Blake <Joshi_neuwirth@yahoo.de>
2020-01-13 21:02:32 +0100

    madness

--- src/speicherverwaltung/memoryManager/WorstFit.java
+++ src/speicherverwaltung/memoryManager/WorstFit.java
@@ -4,8 +4,8 @@ import speicherverwaltung.processManager.Process;
 
 public class WorstFit extends MemoryManager {
 
-	private int[] memory = new int[size]; //Memory als array
-	private int[] gapTable = new int [2];    // [Lückenanfang] [Lückengröße]
+	private int[] memory; //Memory als array
+	private int[][] gapTable = new int [10][2];    // [Lückenanfang] [Lückengröße]
 	/*
 	 * Constructor baut den Speicher, hier: int array [cell] in der größe size [1024] mit dem Eintrag "-1" -> frei
 	 * kommt ein prozess, wird die ID in die cell geschrieben
@@ -13,15 +13,56 @@ public class WorstFit extends MemoryManager {
 	 */
 	public WorstFit(int size) {
 		super(size);
-
+		memory = new int[size];
+		//setzt felder frei
 		for(int i : memory){
 			memory[i] = -1;
 		}
+		setGapTable();
+		printGapTable();
 	}
 
+	private void printGapTable() {
+		for(int[] i : gapTable){
+			System.out.print();
+		}
+	}
+
+	private void setGapTable(){
+		int c = 0;
+		int gapStart = 0;
+		int gapEnd = 0;
+		int gapIndex = 0;
+		boolean toggle = false;
+		int size = getMemorySize();
+		while(c < size){
+			if(memory[c] == -1){
+				if(!toggle){
+					gapStart = c;
+					gapEnd = gapStart;
+					toggle = true;
+				}else{
+					gapEnd++;
+				}
+			}else{
+				if (toggle){
+					gapTable[gapIndex][0] = gapStart;
+					gapTable[gapIndex][1] = gapEnd-gapStart;
+					gapIndex++;
+					if(gapIndex >= gapTable.length){
+						System.err.println("GapTable overflow!");
+						return;
+					}
+					toggle = false;
+				}
+			}
+			c++;
+		}
+	}
 	//reserviert den speicher
 	@Override
 	public boolean allocateMemory(Process process) {
+
 		return false;
 	}
 
